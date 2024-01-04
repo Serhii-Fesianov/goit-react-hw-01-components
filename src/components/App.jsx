2c636ed23f35ade2f1968b61f977675895a3ba5c
Serhii-Fesianov <littlefox06042000@gmail.com>
2024-01-04 15:46:19 +0200

    try 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,11 @@
-import { WrapperCard } from './profile/Profile';
-import user from './data/user.json';
-import data from './data/data.json';
-import friends from './data/friends.json';
-import transactions from './data/transactions.json';
-import { CardStatistics } from './statistics/Statistics';
-import { FriendListUser } from './friends/FriendList';
-import { TransactionsUser } from './transaction/TransactionHistory';
+import { WrapperCard } from './Profile/Profile';
+import user from './Data/user.json';
+import data from './Data/data.json';
+import friends from './Data/friends.json';
+import transactions from './Data/transactions.json';
+import { CardStatistics } from './StatisticsList/Statistics';
+import { FriendListUser } from './FriendsList/FriendList';
+import { TransactionsUser } from './TransactionTable/TransactionHistory';
 
 export const App = () => {
   return (
